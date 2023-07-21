33eee21fb0224878863b2c53d3cceb486f7b1b0f
Prithiga-k <124652825+Prithiga-k@users.noreply.github.com>
2023-07-21 14:03:10 +0530

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,34 @@
+class Monkey{
+     void jump(){
+        System.out.println("Monkey is jumping");
+    }
+    void bite(){
+        System.out.println("Monkey will bite you");
+    }
+}
+
+interface Basic_animal{
+    void eat();
+    void sleep();
+}
+
+class Human extends Monkey implements Basic_animal{
+    void think(){
+        System.out.println("Human can only think");
+    }
+    public void eat(){
+        System.out.println("Human and Monkey can Eat");
+    }
+    public void sleep(){
+        System.out.println("Human and Monkey can Sleep");
+    }
+}
+
+class Animal{
+    public static void main(String args[]){
+        Human Elon = new Human();
+        Elon.eat();
+        Elon.bite();
+        Elon.think();
+    }
+}
